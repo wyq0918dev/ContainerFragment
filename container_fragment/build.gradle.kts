cf1f7a3eda4af8b7c94210b2d51e10b7a8dc83e3
王泳淇 <815099246@qq.com>
2024-02-14 13:50:18 +0800

    commit

--- container_fragment/build.gradle.kts
+++ container_fragment/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
     namespace = "com.wyq0918dev.container_fragment"
     compileSdk = 34
-
     defaultConfig {
         minSdk = 24
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -22,6 +21,16 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+            }
+        }
     }
 }
 
